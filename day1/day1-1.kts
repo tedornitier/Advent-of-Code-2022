bdd3ee7b98f0c2263a00345c30e5c4447fec4ea8
Alessandro Tedesco <spam.alessandrotedd@gmail.com>
2022-12-02 15:58:59 +0100

    Day 1 pt 1

---
+++ day1/day1-1.kts
@@ -0,0 +1,13 @@
+import java.io.File
+
+val input = File("input").readLines().joinToString("\n")
+
+val sums = mutableListOf<Int>()
+input.split("\n\n").forEach {
+    val sum = it.trim()
+        .split("\n")
+        .map { it.toInt() }
+        .sum()
+    sums.add(sum)
+}
+println(sums.sortedDescending().take(1).sum())
